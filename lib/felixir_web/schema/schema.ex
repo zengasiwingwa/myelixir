651abdea038f6a6d1fd07e661a7de89fe4234f09
Zenga Siwingwa <zenga.siwingwa@gmail.com>
2022-02-12 08:11:42 -0500

    refactoring and creating the types

--- lib/felixir_web/schema/schema.ex
+++ lib/felixir_web/schema/schema.ex
@@ -1,8 +1,9 @@
 defmodule FelixirWeb.Schema do
   use Absinthe.Schema
 
-  @desc "query"
+
   query do
+    @desc "greeting"
     field :hello, :string do
       resolve(fn _, _, _ -> {:ok, "world"} end)
     end

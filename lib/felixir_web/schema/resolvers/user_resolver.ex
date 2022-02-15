8a15bd08531a356300a28c2c7f46a43e622b1335
Zenga Siwingwa <zenga.siwingwa@gmail.com>
2022-02-15 17:53:10 -0500

    graphql routes are protected

--- lib/felixir_web/schema/resolvers/user_resolver.ex
+++ lib/felixir_web/schema/resolvers/user_resolver.ex
@@ -1,7 +1,9 @@
 defmodule FelixirWeb.Schema.Resolvers.UserResolver do
   alias Felixir.Auth
 
-  def get_all_users(_, _, _) do
+  def get_all_users(_, _, %{context: context}) do
+    IO.puts("Grabbing the xonrt")
+    IO.inspect(context)
     {:ok, Auth.list_users()}
   end
 end
